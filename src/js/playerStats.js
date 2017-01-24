a4e80d3557370eca5dbd8b5545b2db1faa9bfacb
Luke Hammond <luke.john.hammond@gmail.com>
2017-01-24 17:53:01 +0000

    Basic .gitIgnore, JS pulling out correct data from JSON

--- src/js/playerStats.js
+++ src/js/playerStats.js
@@ -1,6 +1,38 @@
-console.log('Works');
+const components = {
+  initPlayerStats: function (playerId, componentId) {
+    $.getJSON('/data/player-stats.json')
+      .done((data) => {
+        data.players.filter((item) => {
+          if(item.player.id === playerId) {
+            this.playerStats = {
+              currentPlayer: item
+            };
+          }
+
+          this.playerStats.findStatHolders =  document.getElementById(componentId).querySelectorAll('section.playerStat');
+          this.playerStats.attributes = [
+            this.playerStats.currentPlayer.stats[6].value,
+            this.playerStats.currentPlayer.stats[0].value,
+            this.playerStats.currentPlayer.stats[5].value,
+            parseInt(this.playerStats.currentPlayer.stats[0].value/this.playerStats.currentPlayer.stats[6].value*100)/100,
+            parseInt((this.playerStats.currentPlayer.stats[8].value + this.playerStats.currentPlayer.stats[4].value)/this.playerStats.currentPlayer.stats[7].value*100)/100
+          ];
+
+          this.playerStats.findStatHolders.forEach((item, i)=> {
+            item.querySelectorAll('.statValue')[0].innerHTML = this.playerStats.attributes[i];
+          });
+
+          console.log(this);
+        });
+      })
+      .fail((jqxhr, textStatus, error) => {
+        var err = textStatus + ',' + error;
+        console.log( 'Request Failed: ' + err );
+      });
+  }
+};
 
 
-$.getJSON('/data/player-stats.json', (data) => {
-  console.log(data.players[0]);
+document.addEventListener('DOMContentLoaded', () => {
+  components.initPlayerStats(4916, 'stats1');
 });
